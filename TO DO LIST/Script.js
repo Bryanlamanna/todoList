c5dc2a83d8d37fac5c218c38521ed4e92fc72eef
Bryan Lamanna <lamannabryan@gmail.com>
2023-10-04 21:08:32 -0300

    Upload do projeto

---
+++ TO DO LIST/Script.js
@@ -0,0 +1,32 @@
+const localStorageKey = 'to-do-list';
+
+function newTask() {
+    var campoTask = document.getElementById("newTaskInput");
+    var task = campoTask.value;
+
+    if (!task){
+        alert('insira a descrição da tarefa!')
+    } else {
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+        values.push({
+            name: campoTask.value
+        });
+        localStorage.setItem(localStorageKey, JSON.stringify(values));
+    }
+    showTask();
+    limpaCampo();
+}
+
+function limpaCampo() {
+    document.getElementById("newTaskInput").value = '';
+}
+
+function showTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let list = document.getElementById("todolist")
+    list.innerHTML = '';
+
+    for (var i = 0 ; i < values.length; i++) {
+        list.innerHTML += `<li>${values[i]['name']}<button class="okBt">ok</button></li>`;
+    }
+}
