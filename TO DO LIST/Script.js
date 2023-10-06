d3046f90f2492151b559042217bca4087d93a178
Bryanlamanna <107689206+Bryanlamanna@users.noreply.github.com>
2023-10-05 22:54:11 -0300

    more animation improvements

--- TO DO LIST/Script.js
+++ TO DO LIST/Script.js
@@ -23,11 +23,56 @@ function limpaCampo() {
 
 function showTask() {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let list = document.getElementById("todolist")
-    list.innerHTML = '';
+    let list = document.getElementById("todolist");
 
-    for (var i = 0 ; i < values.length; i++) {
-        list.innerHTML += `<li class="task">${values[i]['name']}<button class="okBt">ok</button></li>`;
+    if (values.length === 0) {
+        // Se não houver tarefas, oculta a lista
+        list.style.display = 'none';
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'none';
+    } else {
+        list.style.display = 'block'; // Exibe a lista
+        list.innerHTML = '';
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'block';
+
+        for (var i = 0; i < values.length; i++) {
+            list.innerHTML += `<li class="task">${values[i]['name']}<button class="okBt">✔</button></li>`;
+        }
+        
     }
+    limpaCampo();
+}
+
+function clearTasks() {
+    localStorage.removeItem(localStorageKey);
+
+    // Chama showTask() para ocultar a lista
+    showTask();
+    limpaCampo();
+}
+
+window.onload = function showTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let list = document.getElementById("todolist");
+    
+    if (values.length === 0) {
+        // Se não houver tarefas, oculta a lista
+        list.style.display = 'none';
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'none';
+    } else {
+        list.style.display = 'block'; // Exibe a lista
+        list.innerHTML = '';
+
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'block';
+
+        for (var i = 0; i < values.length; i++) {
+            list.innerHTML += `<li class="task">${values[i]['name']}<button class="okBt">ok</button></li>`;
+        }
+        
+    }
+    limpaCampo();
 }
 
