7cc30d03f397136c97ca080fda015d38e86197f9
Bryanlamanna <107689206+Bryanlamanna@users.noreply.github.com>
2023-10-07 19:58:19 -0300

    finalize hover animations and creat a functional 'donetask' list

--- TO DO LIST/Script.js
+++ TO DO LIST/Script.js
@@ -1,59 +1,15 @@
-const localStorageKey = 'to-do-list';
 
-function newTask() {
-    var campoTask = document.getElementById("newTaskInput");
-    var task = campoTask.value;
+//CONSTANTE PARA DEFINIR O NOME DA KEY NO INTERNAL STORAGE
 
-    if (!task){
-        alert('insira a descrição da tarefa!')
-    } else {
-        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-        values.push({
-            name: campoTask.value
-        });
-        localStorage.setItem(localStorageKey, JSON.stringify(values));
-    }
-    showTask();
-    limpaCampo();
-}
+const localStorageKey1 = 'to-do-list';
+const localStorageKey2 = 'done-list';
 
-function limpaCampo() {
-    document.getElementById("newTaskInput").value = '';
-}
 
-function showTask() {
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let list = document.getElementById("todolist");
 
-    if (values.length === 0) {
-        // Se não houver tarefas, oculta a lista
-        list.style.display = 'none';
-        let clearBtn = document.getElementById("clearTasks");
-        clearBtn.style.display = 'none';
-    } else {
-        list.style.display = 'block'; // Exibe a lista
-        list.innerHTML = '';
-        let clearBtn = document.getElementById("clearTasks");
-        clearBtn.style.display = 'block';
+// METODOS QUE DEVEM INCIAR AO CARREGAR OU RECARREGAR A PAGINA 
 
-        for (var i = 0; i < values.length; i++) {
-            list.innerHTML += `<li class="task">${values[i]['name']}<button class="okBt">✔</button></li>`;
-        }
-        
-    }
-    limpaCampo();
-}
-
-function clearTasks() {
-    localStorage.removeItem(localStorageKey);
-
-    // Chama showTask() para ocultar a lista
-    showTask();
-    limpaCampo();
-}
-
-window.onload = function showTask() {
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+window.load = function showTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey1) || "[]");
     let list = document.getElementById("todolist");
     
     if (values.length === 0) {
@@ -69,10 +25,137 @@ window.onload = function showTask() {
         clearBtn.style.display = 'block';
 
         for (var i = 0; i < values.length; i++) {
-            list.innerHTML += `<li class="task">${values[i]['name']}<button class="okBt">ok</button></li>`;
+            list.innerHTML += `<li class="task">${values[i]['name']}<button onclick="doTask('${values[i]['name']}')" class="okBt">✔</button></li>`;
         }
         
     }
     limpaCampo();
 }
 
+window.onload = function showDoneTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey2) || "[]");
+    let list = document.getElementById("donelist");
+    
+        list.style.display = 'block'; 
+        list.innerHTML = '';
+       
+        for (var i = 0; i < values.length; i++) {
+            list.innerHTML += `<li class="task">${values[i]['name']}</li>`;
+        }
+        
+}
+
+// METODO PARA ADICIONAR UMA NOVA TASK NA INTERNAL STORAGE
+
+function newTask() {
+    var campoTask = document.getElementById("newTaskInput");
+    var task = campoTask.value;
+
+    if (!task){
+        alert('insira a descrição da tarefa!')
+    } else {
+        let values = JSON.parse(localStorage.getItem(localStorageKey1) || "[]");
+        values.push({
+            name: campoTask.value
+        });
+        localStorage.setItem(localStorageKey1, JSON.stringify(values));
+    }
+    showTask();
+    limpaCampo();
+}
+
+//METODO PARA MONTAR PAINEL COM LISTA DE TASKS DA INTERNAL STORAGE
+
+function showTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey1) || "[]");
+    let list = document.getElementById("todolist");
+    
+    if (values.length === 0) {
+        // Se não houver tarefas, oculta a lista
+        list.style.display = 'none';
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'none';
+    } else {
+        list.style.display = 'block'; // Exibe a lista
+        list.innerHTML = '';
+
+        let clearBtn = document.getElementById("clearTasks");
+        clearBtn.style.display = 'block';
+
+        for (var i = 0; i < values.length; i++) {
+            list.innerHTML += `<li class="task">${values[i]['name']}<button onclick="doTask('${values[i]['name']}')" class="okBt">✔</button></li>`;
+        }
+        
+    }
+    limpaCampo();
+}
+
+
+//METODO PARA REMOVER UMA TASK DO INTERNAL STORAGE
+
+function doTask(data) {
+
+    let values = JSON.parse(localStorage.getItem(localStorageKey1) || "[]");
+    let index = values.findIndex(x => x.name == data);
+    if (index !== -1) {
+        values.splice(index, 1);
+        localStorage.setItem(localStorageKey1, JSON.stringify(values));
+        showTask(); // Chame showTask() somente após a remoção bem-sucedida
+        newDoneTask(data);
+    }
+}
+
+//METODO PARA MOSTRAR AS TASKS CONCLUIDAS
+
+function showDoneTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey2) || "[]");
+    let list = document.getElementById("donelist");
+     
+        if (values.length === 0) {
+            // Se não houver tarefas, oculta a lista
+            list.style.display = 'none';
+            let clearBtn = document.getElementById("clearTasks");
+            clearBtn.style.display = 'none';
+        } else {
+            list.style.display = 'block'; // Exibe a lista
+            list.innerHTML = '';
+    
+            let clearBtn = document.getElementById("clearTasks");
+            clearBtn.style.display = 'block';
+       
+        for (var i = 0; i < values.length; i++) {
+            list.innerHTML += `<li class="task">${values[i]['name']}</li>`;
+        }
+    } 
+    }
+    
+//METODO PARA TRANSFERIR UMA TASK PARA LISTA DE CONCLUIDAS
+
+function newDoneTask(data) {
+        let values = JSON.parse(localStorage.getItem(localStorageKey2) || "[]");
+        values.push({
+            name: data
+        });
+        localStorage.setItem(localStorageKey2, JSON.stringify(values));
+    
+    showDoneTask();
+    limpaCampo();
+}
+
+//METODO PARA LIMPAR AS TASKS DO INTERNAL STORAGE
+
+function clearTasks() {
+    localStorage.removeItem(localStorageKey1);
+
+    localStorage.removeItem(localStorageKey2);
+// Chama showTask() para ocultar a lista
+showTask();
+showDoneTask();
+
+}
+
+//METODO PARA LIMPAR O CAMPO INPUT APÓS REGISTRAR TASKS
+
+function limpaCampo() {
+    document.getElementById("newTaskInput").value = '';
+}
